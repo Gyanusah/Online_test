2161cbbaf1c2fe86cebfc8a638169465186817c7
Gyanendra Sah <shahgyanendra555@gmail.com>
2026-08-04 22:42:30 +0545

    first commit

--- client/src/pages/auth/Signup.jsx
+++ client/src/pages/auth/Signup.jsx
@@ -462,12 +462,12 @@ const Signup = () => {
               </span>
             </div>
 
-            <button
+            {/* <button
               type="submit"
               className="w-full py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-medium"
             >
               Create Account
-            </button>
+            </button> */}
           </form>
 
           <div className="mt-6 text-center">
